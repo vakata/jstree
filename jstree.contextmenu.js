f499865e4510325816d98d0c9574d0056a35b763
Ivan Bozhanov <github@vakata.com>
2011-07-12 18:15:19 +0300

    added: contextmenu, hotkeys, html, sort (need to be tested) + a few fixes

---
+++ jstree.contextmenu.js
@@ -0,0 +1,119 @@
+/* File: jstree.sort.js 
+Sorts items alphabetically (or using any other function)
+*/
+/* Group: jstree sort plugin */
+(function ($) {
+	$.jstree.plugin("contextmenu", {
+		__construct : function () {
+			this.get_container()
+				.delegate("a", "contextmenu.jstree", $.proxy(function (e) {
+						e.preventDefault();
+						if(!this.is_loading(e.currentTarget)) {
+							this.show_contextmenu(e.currentTarget, e.pageX, e.pageY);
+						}
+					}, this))
+				.delegate("a", "click.jstree", $.proxy(function (e) {
+						if(this.data.contextmenu.visible) {
+							this._hide_contextmenu();
+						}
+					}, this));
+			$(document).bind("context_hide.vakata", $.proxy(function () { this.data.contextmenu = false; }, this));
+		},
+		__destruct : function () {
+			if(this.data.contextmenu) {
+				this._hide_contextmenu();
+			}
+		},
+		defaults : { 
+			select_node : true, 
+			show_at_node : true,
+			items : { // Could be a function that should return an object like this one
+				"create" : {
+					"separator_before"	: false,
+					"separator_after"	: true,
+					"label"				: "Create",
+					"action"			: function (data) { }
+				},
+				"rename" : {
+					"separator_before"	: false,
+					"separator_after"	: false,
+					"label"				: "Rename",
+					"action"			: function (data) { }
+				},
+				"remove" : {
+					"separator_before"	: false,
+					"icon"				: false,
+					"separator_after"	: false,
+					"label"				: "Delete",
+					"action"			: function (data) {
+						var inst = $.jstree._reference(data.reference),
+							obj = inst.get_node(data.reference);
+						inst.delete_node(obj);
+					}
+				},
+				"ccp" : {
+					"separator_before"	: true,
+					"icon"				: false,
+					"separator_after"	: false,
+					"label"				: "Edit",
+					"action"			: false,
+					"submenu" : { 
+						"cut" : {
+							"separator_before"	: false,
+							"separator_after"	: false,
+							"label"				: "Cut",
+							"action"			: function (data) { }
+						},
+						"copy" : {
+							"separator_before"	: false,
+							"icon"				: false,
+							"separator_after"	: false,
+							"label"				: "Copy",
+							"action"			: function (data) { }
+						},
+						"paste" : {
+							"separator_before"	: false,
+							"icon"				: false,
+							"separator_after"	: false,
+							"label"				: "Paste",
+							"action"			: function (data) { }
+						}
+					}
+				}
+			}
+		},
+		_fn : {
+			show_contextmenu : function (obj, x, y) {
+				obj = this.get_node(obj);
+				var s = this.get_settings().contextmenu,
+					a = obj.children("a:visible:eq(0)"),
+					o = false,
+					i = false;
+				if(s.show_at_node || typeof x === "undefined" || typeof y === "undefined") {
+					o = a.offset();
+					x = o.left;
+					y = o.top + this.data.core.li_height;
+				}
+				if(s.select_node && this.data.ui && !this.is_selected(obj)) {
+					this.deselect_all();
+					this.select_node(obj);
+				}
+
+				i = obj.data("jstree") && obj.data("jstree").contextmenu ? obj.data("jstree").contextmenu : s.items;
+				if($.isFunction(i)) { i = i.call(this, obj); }
+
+				$(document).one("context_show.vakata", $.proxy(function (e, data) { 
+					var cls = 'jstree-contextmenu';
+					if(this.data.themes.theme) {
+						cls += ' jstree-' + this.data.themes.theme + '-contextmenu';
+					}
+					$(data.element).addClass(cls);
+				}, this));
+				this.data.contextmenu.visible = true;
+				$.vakata.context.show(a, { 'x' : x, 'y' : y }, i);
+				this.__callback({ "obj" : obj, "x" : x, "y" : y });
+			}
+		}
+	});
+	$.jstree.defaults.plugins.push("contextmenu");
+})(jQuery);
---
+++ jstree.html.js
@@ -0,0 +1,68 @@
+/* File: jstree.html.js 
+This plugin makes it possible for jstree to use HTML data sources (other than the container's initial HTML).
+*/
+/* Group: jstree html plugin */
+(function ($) {
+	$.jstree.plugin("html", {
+		defaults : {
+			data	: false,
+			ajax	: false
+		},
+		_fn : { 
+			_append_html_data : function (dom, data) {
+				dom = this.get_node(dom);
+				if(dom === -1) { dom = this.get_container(); }
+				data = $(data);
+				if(!data || !data.length || !dom.length, !data.is('ul, li')) { return false; }
+				if(!dom.children('ul').length) { dom.append('<ul />'); }
+				dom.children('ul').empty().append(data.is('ul') ? data.children('li') : data);
+				return true;
+			},
+			_load_node : function (obj, callback) {
+				var d = false,
+					s = this.get_settings().html;
+				obj = this.get_node(obj);
+				if(!obj) { return false; }
+
+				switch(!0) {
+					// no settings - user original html
+					case (!s.data && !s.ajax): 
+						if(obj === -1) {
+							this._append_html_data(-1, this.data.core.original_container_html.clone(true));
+						}
+						return callback.call(this, true);
+					// data is function
+					case ($.isFunction(s.data)):
+						return s.data.call(this, obj, $.proxy(function (d) {
+							return callback.call(this, this._append_html_data(obj, d));
+						}, this));
+					// data is set, ajax is not set, or both are set, but we are dealing with root node
+					case ((!!s.data && !s.ajax) || (!!s.data && !!s.ajax && obj === -1)):
+						return callback.call(this, this._append_html_data(obj, s.data));
+					// data is not set, ajax is set, or both are set, but we are dealing with a normal node
+					case ((!s.data && !!s.ajax) || (!!s.data && !!s.ajax && obj !== -1)):
+						s.ajax.success = $.proxy(function (d, t, x) { 
+							var s = this.get_settings().html.ajax;
+							if($.isFunction(s.success)) {
+								d = s.success.call(this, d, t, x) || d;
+							}
+							callback.call(this, this._append_html_data(obj, d));
+						}, this);
+						s.ajax.error = $.proxy(function (x, t, e) { 
+							var s = this.get_settings().html.ajax;
+							if($.isFunction(s.error)) {
+								s.error.call(this, x, t, e);
+							}
+							callback.call(this, false);
+						}, this);
+						if(!s.ajax.dataType) { s.ajax.dataType = "html"; }
+						if($.isFunction(s.ajax.url))	{ s.ajax.url	= s.ajax.url.call(this, obj); }
+						if($.isFunction(s.ajax.data))	{ s.ajax.data	= s.ajax.data.call(this, obj); }
+						return $.ajax(s.ajax);
+				}
+			}
+		}
+	});
+	// include the html plugin by default
+	$.jstree.defaults.plugins.push("html");
+})(jQuery);
