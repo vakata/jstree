86b87dc7ba88d5b4baba7787694ccbad749a2602
Ivan Bozhanov <github@vakata.com>
2011-07-26 23:51:53 +0300

    added hover to open in DND plugin, added multiple checks while dragging to find any allowed position for the dragged node

--- jstree.dnd.js
+++ jstree.dnd.js
@@ -14,20 +14,25 @@ Enables drag'n'drop.
 					}
 				}, this));
 		},
+		// TODO: is check_timeout or is it OK as is?
 		defaults : {
-			copy_modifier : 'ctrl'
+			copy_modifier : 'ctrl',
+			open_timeout : 500
 		}
 	});
 
 	$(function() {
 		// bind only once for all instances
 		var lastmv = false,
+			opento = false,
 			marker = $('<div id="jstree-marker">&#160;</div>').hide().appendTo('body');
+
 		$(document)
 			.bind('dnd_start.vakata', function (e, data) { 
 				lastmv = false;
 			})
 			.bind('dnd_move.vakata', function (e, data) { 
+				if(opento) { clearTimeout(opento); }
 				if(!data.data.jstree) { return; }
 
 				// if we are hovering the marker image do nothing (can happen on "inside" drags)
@@ -39,7 +44,7 @@ Enables drag'n'drop.
 					ref = false,
 					off = false,
 					rel = false,
-					l, t, h, p, i;
+					l, t, h, p, i, o;
 				// if we are over an instance
 				if(ins && ins.data && ins.data.dnd) {
 					marker.attr('class', (ins.data.themes ? 'jstree-' + ins.get_theme() : ''));
@@ -65,29 +70,47 @@ Enables drag'n'drop.
 							rel = data.event.pageY - off.top;
 							h = ref.height();
 							if(rel < h / 3) { 
-								l = off.left - 6;
-								t = off.top - 5;
-								p = ins.get_parent(ref);
-								i = ref.parent().index();
+								o = ['b', 'i', 'a'];
 							}
 							else if(rel > h - h / 3) {
-								l = off.left - 6;
-								t = off.top - 5 + h + 2;
-								p = ins.get_parent(ref);
-								i = ref.parent().index() + 1;
+								o = ['a', 'i', 'b'];
 							}
 							else {
-								l = off.left - 2;
-								t = off.top - 5 + h / 2 + 1;
-								p = ref.parent();
-								i = 'last';
+								o = rel > h / 2 ? ['i', 'a', 'b'] : ['i', 'b', 'a'];
 							}
-							if(ins.check_move(data.data.obj, p, i)) {
-								lastmv = { 'ins' : ins, 'par' : p, 'pos' : i };
-								marker.css({ 'left' : l + 'px', 'top' : t + 'px' }).show();
-								data.helper.find('.jstree-icon:eq(0)').removeClass('jstree-er').addClass('jstree-ok');
-								return;
-							}
+							$.each(o, function (j, v) {
+								switch(v) {
+									case 'b':
+										l = off.left - 6;
+										t = off.top - 5;
+										p = ins.get_parent(ref);
+										i = ref.parent().index();
+										break;
+									case 'i':
+										l = off.left - 2;
+										t = off.top - 5 + h / 2 + 1;
+										p = ref.parent();
+										i = 'last';
+										break;
+									case 'a':
+										l = off.left - 6;
+										t = off.top - 5 + h + 2;
+										p = ins.get_parent(ref);
+										i = ref.parent().index() + 1;
+										break;
+								}
+								if(ins.check_move(data.data.obj, p, i)) {
+									if(v === 'i' && ref.parent().is('.jstree-closed') && ins.get_settings(true).dnd.open_timeout) {
+										opento = setTimeout((function (x, z) { return function () { x.open_node(z); }; })(ins, ref), ins.get_settings(true).dnd.open_timeout);
+									}
+									lastmv = { 'ins' : ins, 'par' : p, 'pos' : i };
+									marker.css({ 'left' : l + 'px', 'top' : t + 'px' }).show();
+									data.helper.find('.jstree-icon:eq(0)').removeClass('jstree-er').addClass('jstree-ok');
+									o = true;
+									return false;
+								}
+							});
+							if(o === true) { return; }
 						}
 					}
 				}
@@ -102,6 +125,7 @@ Enables drag'n'drop.
 				data.helper.find('.jstree-icon:eq(0)').removeClass('jstree-ok').addClass('jstree-er');
 			})
 			.bind('dnd_stop.vakata', function (e, data) { 
+				if(opento) { clearTimeout(opento); }
 				if(!data.data.jstree) { return; }
 				marker.hide();
 				if(lastmv) {
@@ -126,119 +150,6 @@ Enables drag'n'drop.
 				'#jstree-dnd .jstree-copy { margin:0 2px 0 2px; }';
 		$.vakata.css.add_sheet({ str : css_string, title : "jstree" });
 	});
-	// include the sort plugin by default
+	// include the dnd plugin by default
 	$.jstree.defaults.plugins.push("dnd");
 })(jQuery);
-
-
-// TODO: open_timeout, check_timeout, check all options in order
-
-
-
-
-/*
-
-(function ($) { 
-	return;
-
-	$.jstree.plugin("dnd", {
-		defaults : {
-			copy_modifier	: "ctrl",
-			check_timeout	: 100,
-			open_timeout	: 500,
-		},
-
-			dnd_show : function () {
-				if(!this.data.dnd.prepared) { return; }
-				var o = ["before","inside","after"],
-					r = false,
-					rtl = this._get_settings().core.rtl,
-					pos;
-				if(this.data.dnd.w < this.data.core.li_height/3) { o = ["before","inside","after"]; }
-				else if(this.data.dnd.w <= this.data.core.li_height*2/3) {
-					o = this.data.dnd.w < this.data.core.li_height/2 ? ["inside","before","after"] : ["inside","after","before"];
-				}
-				else { o = ["after","inside","before"]; }
-				$.each(o, $.proxy(function (i, val) { 
-					if(this.data.dnd[val]) {
-						$.vakata.dnd.helper.children("ins").attr("class","jstree-ok");
-						r = val;
-						return false;
-					}
-				}, this));
-				if(r === false) { $.vakata.dnd.helper.children("ins").attr("class","jstree-invalid"); }
-				
-				pos = rtl ? (this.data.dnd.off.right - 18) : (this.data.dnd.off.left + 10);
-				switch(r) {
-					case "before":
-						m.css({ "left" : pos + "px", "top" : (this.data.dnd.off.top - 6) + "px" }).show();
-						if(ml) { ml.css({ "left" : (pos + 8) + "px", "top" : (this.data.dnd.off.top - 1) + "px" }).show(); }
-						break;
-					case "after":
-						m.css({ "left" : pos + "px", "top" : (this.data.dnd.off.top + this.data.core.li_height - 6) + "px" }).show();
-						if(ml) { ml.css({ "left" : (pos + 8) + "px", "top" : (this.data.dnd.off.top + this.data.core.li_height - 1) + "px" }).show(); }
-						break;
-					case "inside":
-						m.css({ "left" : pos + ( rtl ? -4 : 4) + "px", "top" : (this.data.dnd.off.top + this.data.core.li_height/2 - 5) + "px" }).show();
-						if(ml) { ml.hide(); }
-						break;
-					default:
-						m.hide();
-						if(ml) { ml.hide(); }
-						break;
-				}
-				last_pos = r;
-				return r;
-			},
-			dnd_open : function () {
-				this.data.dnd.to2 = false;
-				this.open_node(r, $.proxy(this.dnd_prepare,this), true);
-			},
-			dnd_finish : function (e) {
-				if(this.data.dnd.foreign) {
-					if(this.data.dnd.after || this.data.dnd.before || this.data.dnd.inside) {
-						this._get_settings().dnd.drag_finish.call(this, { "o" : o, "r" : r, "p" : last_pos });
-					}
-				}
-				else {
-					this.dnd_prepare();
-					this.move_node(o, r, last_pos, e[this._get_settings().dnd.copy_modifier + "Key"]);
-				}
-				o = false;
-				r = false;
-				m.hide();
-				if(ml) { ml.hide(); }
-			},
-			dnd_enter : function (obj) {
-				if(this.data.dnd.mto) { 
-					clearTimeout(this.data.dnd.mto);
-					this.data.dnd.mto = false;
-				}
-				var s = this._get_settings().dnd;
-				this.data.dnd.prepared = false;
-				r = this._get_node(obj);
-				if(s.check_timeout) { 
-					// do the calculations after a minimal timeout (users tend to drag quickly to the desired location)
-					if(this.data.dnd.to1) { clearTimeout(this.data.dnd.to1); }
-					this.data.dnd.to1 = setTimeout($.proxy(this.dnd_prepare, this), s.check_timeout); 
-				}
-				else { 
-					this.dnd_prepare(); 
-				}
-				if(s.open_timeout) { 
-					if(this.data.dnd.to2) { clearTimeout(this.data.dnd.to2); }
-					if(r && r.length && r.hasClass("jstree-closed")) { 
-						// if the node is closed - open it, then recalculate
-						this.data.dnd.to2 = setTimeout($.proxy(this.dnd_open, this), s.open_timeout);
-					}
-				}
-				else {
-					if(r && r.length && r.hasClass("jstree-closed")) { 
-						this.dnd_open();
-					}
-				}
-			}
-		}
-	});
-})(jQuery);
-//*/
