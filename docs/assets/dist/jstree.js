80ee3c7246e3e00c89df89ec929c0d6dc667655d
Ivan Bozhanov <git@vakata.com>
2014-01-20 19:16:55 +0200

    converted to amd anonymous module - close #488

--- docs/assets/dist/jstree.js
+++ docs/assets/dist/jstree.js
@@ -2,7 +2,7 @@
 (function (factory) {
 	"use strict";
 	if (typeof define === 'function' && define.amd) {
-		define('jstree', ['jquery'], factory);
+		define(['jquery'], factory);
 	}
 	else if(typeof exports === 'object') {
 		factory(require('jquery'));
