32dab75ad3ace4d754ad7bb59f6ea1b00e1a38b0
Ivan Bozhanov <git@vakata.com>
2014-02-10 18:23:15 +0200

    beta6

--- docs/assets/dist/jstree.js
+++ docs/assets/dist/jstree.js
@@ -67,7 +67,7 @@
 		 * specifies the jstree version in use
 		 * @name $.jstree.version
 		 */
-		version : '3.0.0-beta5',
+		version : '3.0.0-beta6',
 		/**
 		 * holds all the default options used when creating new instances
 		 * @name $.jstree.defaults
