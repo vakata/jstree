dedeaa77416de04f6c8b3d2c2eabc463a21b07ef
Ivan Bozhanov <git@vakata.com>
2014-03-10 15:31:50 +0200

    further fixes for invalid IDs

--- src/jstree.search.js
+++ src/jstree.search.js
@@ -193,7 +193,7 @@
 		this._search_open = function (d) {
 			var t = this;
 			$.each(d.concat([]), function (i, v) {
-				v = $('#' + v, t.element);
+				v = $('#' + v.replace(/[\\:'". \/]/g,'\\$&'), t.element);
 				if(v.length) {
 					if(t.is_closed(v)) {
 						t._data.search.opn.push(v[0].id);
