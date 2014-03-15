e22db21564a5de1b0bc60f13f4b96e45e086d51d
Ivan Bozhanov <git@vakata.com>
2014-03-15 19:19:09 +0200

    beta10

--- src/jstree.js
+++ src/jstree.js
@@ -67,7 +67,7 @@
 		 * specifies the jstree version in use
 		 * @name $.jstree.version
 		 */
-		version : '3.0.0-beta9',
+		version : '3.0.0-beta10',
 		/**
 		 * holds all the default options used when creating new instances
 		 * @name $.jstree.defaults
