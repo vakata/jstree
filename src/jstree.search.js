d5c1cef9cb016e779f69e9ffa2d0bae6f05e3759
Ivan Bozhanov <git@vakata.com>
2014-09-25 17:25:00 +0300

    missed show_only_matches

--- src/jstree.search.js
+++ src/jstree.search.js
@@ -166,7 +166,7 @@
 						.done($.proxy(function (d) {
 							if(d && d.d) { d = d.d; }
 							this._load_nodes(!$.isArray(d) ? [] : $.vakata.array_unique(d), function () {
-								this.search(str, true);
+								this.search(str, true, show_only_matches);
 							}, true);
 						}, this));
 				}
