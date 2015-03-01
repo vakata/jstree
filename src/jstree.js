ec1cec9e3298fa471a80b8cfbfcc622f060527ac
Ivan Bozhanov <git@vakata.com>
2015-03-01 21:49:36 +0200

    no longer processing keyboard events if meta keys are used - close #1016

--- src/jstree.js
+++ src/jstree.js
@@ -610,6 +610,7 @@
 					}, this))
 				.on('keydown.jstree', '.jstree-anchor', $.proxy(function (e) {
 						if(e.target.tagName === "INPUT") { return true; }
+						if(e.which !== 32 && e.which !== 13 && (e.shiftKey || e.ctrlKey || e.altKey || e.metaKey)) { return true; }
 						var o = null;
 						if(this._data.core.rtl) {
 							if(e.which === 37) { e.which = 39; }
