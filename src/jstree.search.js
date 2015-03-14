6bfdfa1a83d33397d760dfff37c84d09f1e31ceb
aj-dev <reskogitas@gmail.com>
2015-03-14 15:19:49 +0100

    fuzzy search improvements + optional search debounce

--- src/jstree.search.js
+++ src/jstree.search.js
@@ -27,10 +27,10 @@
 	 */
 	$.jstree.defaults.search = {
 		/**
-		 * a jQuery-like AJAX config, which jstree uses if a server should be queried for results. 
-		 * 
+		 * a jQuery-like AJAX config, which jstree uses if a server should be queried for results.
+		 *
 		 * A `str` (which is the search string) parameter will be added with the request, an optional `inside` parameter will be added if the search is limited to a node id. The expected result is a JSON array with nodes that need to be opened so that matching nodes will be revealed.
-		 * Leave this setting as `false` to not query the server. You can also set this to a function, which will be invoked in the instance's scope and receive 3 parameters - the search string, the callback to call with the array of nodes to load, and the optional node ID to limit the search to 
+		 * Leave this setting as `false` to not query the server. You can also set this to a function, which will be invoked in the instance's scope and receive 3 parameters - the search string, the callback to call with the array of nodes to load, and the optional node ID to limit the search to
 		 * @name $.jstree.defaults.search.ajax
 		 * @plugin search
 		 */
@@ -48,7 +48,7 @@
 		 */
 		case_sensitive : false,
 		/**
-		 * Indicates if the tree should be filtered (by default) to show only matching nodes (keep in mind this can be a heavy on large trees in old browsers). 
+		 * Indicates if the tree should be filtered (by default) to show only matching nodes (keep in mind this can be a heavy on large trees in old browsers).
 		 * This setting can be changed at runtime when calling the search method. Default is `false`.
 		 * @name $.jstree.defaults.search.show_only_matches
 		 * @plugin search
@@ -72,7 +72,14 @@
 		 * @name $.jstree.defaults.search.search_callback
 		 * @plugin search
 		 */
-		search_callback : false
+		search_callback : false,
+		/**
+		 * Indicates if the search should be delayed/debounced. Value must be a number of milliseconds or `false`.
+		 * Can improve performance when searching large data sets. Default is `false`.
+		 * @name $.jstree.defaults.search.delay
+		 * @plugin search
+		 */
+		delay: false
 	};
 
 	$.jstree.plugins.search = function (options, parent) {
@@ -132,92 +139,241 @@
 		 * @trigger search.jstree
 		 */
 		this.search = function (str, skip_async, show_only_matches, inside, append) {
-			if(str === false || $.trim(str.toString()) === "") {
+			var searchOptions = this.settings.search,
+				search;
+
+			search = $.proxy(this._search, this, str, skip_async, show_only_matches, inside, append);
+
+			if (typeof searchOptions.delay === 'number') {
+				this._debounce(search, searchOptions.delay)(); // debounced search
+				return;
+			}
+
+			search(); // immediate search
+		};
+
+		/**
+		 * Searches tree nodes for a given string. Used only internally.
+		 * @private
+		 * @name _search(str [, skip_async, show_only_matches, inside, append])
+		 * @param {String} str the search string
+		 * @param {Boolean} skip_async if set to true server will not be queried even if configured
+		 * @param {Boolean} show_only_matches if set to true only matching nodes will be shown (keep in mind this can be very slow on large trees or old browsers)
+		 * @param {Mixed} inside an optional node to whose children to limit the search
+		 * @param {Boolean} append if set to true the results of this search are appended to the previous search
+		 * @plugin search
+		 */
+		this._search = function (str, skip_async, show_only_matches, inside, append) {
+			var searchOptions = this.settings.search,
+				ajax = searchOptions.ajax || false,
+				model = this._model.data,
+				matches = [],
+				parents = [],
+				isMatch;
+
+			if (str === false || $.trim(str.toString()) === '') {
 				return this.clear_search();
 			}
-			inside = this.get_node(inside);
-			inside = inside && inside.id ? inside.id : null;
-			str = str.toString();
-			var s = this.settings.search,
-				a = s.ajax ? s.ajax : false,
-				m = this._model.data,
-				f = null,
-				r = [],
-				p = [], i, j;
-			if(this._data.search.res.length && !append) {
+
+			if (this._data.search.res.length && !append) {
 				this.clear_search();
 			}
-			if(show_only_matches === undefined) {
-				show_only_matches = s.show_only_matches;
+
+			if (show_only_matches === undefined) {
+				show_only_matches = searchOptions.show_only_matches;
 			}
-			if(!skip_async && a !== false) {
-				if($.isFunction(a)) {
-					return a.call(this, str, $.proxy(function (d) {
-							if(d && d.d) { d = d.d; }
-							this._load_nodes(!$.isArray(d) ? [] : $.vakata.array_unique(d), function () {
-								this.search(str, true, show_only_matches, inside, append);
-							}, true);
-						}, this), inside);
-				}
-				else {
-					a = $.extend({}, a);
-					if(!a.data) { a.data = {}; }
-					a.data.str = str;
+
+			str = str.toString();
+			inside = this.get_node(inside);
+			inside = inside && inside.id ? inside.id : null;
+
+			if (!skip_async && ajax !== false) {
+				if ($.isFunction(ajax)) {
+					return ajax.call(this, str, $.proxy(function (d) {
+						if (d && d.d) {
+							d = d.d;
+						}
+						this._load_nodes(!$.isArray(d) ? [] : $.vakata.array_unique(d), function () {
+							this.search(str, true, show_only_matches, inside, append);
+						}, true);
+					}, this), inside);
+				} else {
+					ajax = $.extend({}, ajax);
+					if (!ajax.data) {
+						ajax.data = {};
+					}
+					ajax.data.str = str;
 					if(inside) {
 						a.data.inside = inside;
 					}
-					return $.ajax(a)
+					return $.ajax(ajax)
 						.fail($.proxy(function () {
-							this._data.core.last_error = { 'error' : 'ajax', 'plugin' : 'search', 'id' : 'search_01', 'reason' : 'Could not load search parents', 'data' : JSON.stringify(a) };
+							this._data.core.last_error = {
+								'error': 'ajax',
+								'plugin': 'search',
+								'id': 'search_01',
+								'reason': 'Could not load search parents',
+								'data': JSON.stringify(ajax)
+							};
 							this.settings.core.error.call(this, this._data.core.last_error);
 						}, this))
 						.done($.proxy(function (d) {
-							if(d && d.d) { d = d.d; }
+							if (d && d.d) {
+								d = d.d;
+							}
 							this._load_nodes(!$.isArray(d) ? [] : $.vakata.array_unique(d), function () {
 								this.search(str, true, show_only_matches, inside, append);
 							}, true);
 						}, this));
 				}
 			}
-			if(!append) {
-				this._data.search.str = str;
-				this._data.search.dom = $();
-				this._data.search.res = [];
-				this._data.search.opn = [];
-				this._data.search.som = show_only_matches;
-			}
 
-			f = new $.vakata.search(str, true, { caseSensitive : s.case_sensitive, fuzzy : s.fuzzy });
-			$.each(m[inside ? inside : '#'].children_d, function (ii, i) {
-				var v = m[i];
-				if(v.text && ( (s.search_callback && s.search_callback.call(this, str, v)) || (!s.search_callback && f.search(v.text).isMatch) ) && (!s.search_leaves_only || (v.state.loaded && v.children.length === 0)) ) {
-					r.push(i);
-					p = p.concat(v.parents);
+			this._data.search.str = str;
+			this._data.search.dom = $();
+			this._data.search.res = [];
+			this._data.search.opn = [];
+			this._data.search.som = show_only_matches;
+
+			isMatch = this._isMatch(str, {
+				caseSensitive: searchOptions.case_sensitive,
+				fuzzy: searchOptions.fuzzy
+			});
+
+			$.each(model[inside ? inside : '#'].children_d, function (i, id) {
+				var node = model[id];
+
+				if (node.text && ((searchOptions.search_callback && searchOptions.search_callback.call(this, str, node)) ||
+					(!searchOptions.search_callback && isMatch(node.text))) && (!searchOptions.search_leaves_only || (node.state.loaded && node.children.length === 0))) {
+
+					matches.push(id);
+					parents = parents.concat(node.parents);
 				}
 			});
-			if(r.length) {
-				p = $.vakata.array_unique(p);
-				this._search_open(p);
-				if(!append) {
-					this._data.search.dom = $(this.element[0].querySelectorAll('#' + $.map(r, function (v) { return "0123456789".indexOf(v[0]) !== -1 ? '\\3' + v[0] + ' ' + v.substr(1).replace($.jstree.idregex,'\\$&') : v.replace($.jstree.idregex,'\\$&'); }).join(', #')));
-					this._data.search.res = r;
+
+			if (matches.length) {
+				parents = $.vakata.array_unique(parents);
+				this._search_open(parents);
+
+				if (!append) {
+					this._data.search.dom = $(this.element[0].querySelectorAll('#' + $.map(matches, function (v) {
+						return "0123456789".indexOf(v[0]) !== -1 ? '\\3' + v[0] + ' ' + v.substr(1).replace($.jstree.idregex, '\\$&') : v.replace($.jstree.idregex, '\\$&');
+					}).join(', #')));
+					this._data.search.res = matches;
+				} else {
+					this._data.search.dom = this._data.search.dom.add($(this.element[0].querySelectorAll('#' + $.map(matches, function (v) {
+						return "0123456789".indexOf(v[0]) !== -1 ? '\\3' + v[0] + ' ' + v.substr(1).replace($.jstree.idregex,'\\$&') : v.replace($.jstree.idregex,'\\$&');
+					}).join(', #'))));
+					this._data.search.res = $.vakata.array_unique(this._data.search.res.concat(matches));
 				}
-				else {
-					this._data.search.dom = this._data.search.dom.add($(this.element[0].querySelectorAll('#' + $.map(r, function (v) { return "0123456789".indexOf(v[0]) !== -1 ? '\\3' + v[0] + ' ' + v.substr(1).replace($.jstree.idregex,'\\$&') : v.replace($.jstree.idregex,'\\$&'); }).join(', #'))));
-					this._data.search.res = $.vakata.array_unique(this._data.search.res.concat(r));
-				}
-				this._data.search.dom.children(".jstree-anchor").addClass('jstree-search');
+
+				this._data.search.dom.children('.jstree-anchor').addClass('jstree-search');
 			}
+
 			/**
 			 * triggered after search is complete
 			 * @event
 			 * @name search.jstree
 			 * @param {jQuery} nodes a jQuery collection of matching nodes
 			 * @param {String} str the search string
-			 * @param {Array} res a collection of objects represeing the matching nodes
+			 * @param {Array} res a collection of objects representing the matching nodes
 			 * @plugin search
 			 */
-			this.trigger('search', { nodes : this._data.search.dom, str : str, res : this._data.search.res, show_only_matches : show_only_matches });
+			this.trigger('search', {
+				nodes: this._data.search.dom,
+				str: str,
+				res: this._data.search.res,
+				show_only_matches: show_only_matches
+			});
+		};
+
+		/**
+		 * Finds the needle in a haystack where needle is user search query and haystack is jstree node's text property.
+		 * Used only internally.
+		 * @private
+		 * @name _isMatch(needle, options)
+		 * @param {String} needle User search query
+		 * @param {Object} options Search options object
+		 * @return {Boolean}
+		 * @plugin search
+		 */
+		this._isMatch = function (needle, options) {
+			var fuzzy = needle.length > 32 ? false : options.fuzzy,
+				caseSensitive = options.caseSensitive;
+
+			return function (haystack) {
+				var charPos = -1; // remember position of last found character
+
+				haystack = caseSensitive ? haystack : haystack.toLowerCase();
+
+				if (needle === haystack || haystack.indexOf(needle) !== -1) {
+					return true;
+				}
+
+				if (fuzzy) {
+			        // consider each search character one at a time
+			        for (var i = 0; i < needle.length; i++) {
+			            var l = needle[i];
+
+			            if (l === ' ') {
+			            	continue; // ignore spaces
+			            }
+
+			            charPos = haystack.indexOf(l, charPos + 1); // search for character & update position to search from
+
+			            if (charPos === -1) {
+			            	return false;
+			            }
+			        }
+
+					return true;
+				}
+
+				return false;
+			};
+		};
+
+		/**
+		 * Debounce function to delay search, from https://github.com/rhysbrettbowen/debounce. Used only internally.
+		 * @private
+		 * @name _debounce(func, wait)
+		 * @param {Function} func A function to debounce
+		 * @param {Number} wait Number of milliseconds to delay function call
+		 * @return {Function} Function that will run after a specified `wait` period
+		 * @plugin search
+		 */
+		this._debounce = function (func, wait) {
+			// we need to save these in the closure
+			var timeout, args, context, timestamp;
+
+			return function () {
+				// save details of latest call
+				context = this;
+				args = [].slice.call(arguments, 0);
+				timestamp = new Date();
+
+				// this is where the magic happens
+				var later = function () {
+
+					// how long ago was the last call
+					var last = (new Date()) - timestamp;
+
+					// if the latest call was less that the wait period ago
+					// then we reset the timeout to wait for the difference
+					if (last < wait) {
+						timeout = setTimeout(later, wait - last);
+
+					// or if not we can null out the timer and run the latest
+					} else {
+						timeout = null;
+						func.apply(context, args);
+					}
+				};
+
+				// we only need to set the timer now if one isn't already running
+				if (!timeout) {
+					timeout = setTimeout(later, wait);
+				}
+			};
 		};
 		/**
 		 * used to clear the last search (removes classes and shows all nodes if filtering is on)
@@ -266,127 +422,6 @@
 			});
 		};
 	};
-
-	// helpers
-	(function ($) {
-		// from http://kiro.me/projects/fuse.html
-		$.vakata.search = function(pattern, txt, options) {
-			options = options || {};
-			if(options.fuzzy !== false) {
-				options.fuzzy = true;
-			}
-			pattern = options.caseSensitive ? pattern : pattern.toLowerCase();
-			var MATCH_LOCATION	= options.location || 0,
-				MATCH_DISTANCE	= options.distance || 100,
-				MATCH_THRESHOLD	= options.threshold || 0.6,
-				patternLen = pattern.length,
-				matchmask, pattern_alphabet, match_bitapScore, search;
-			if(patternLen > 32) {
-				options.fuzzy = false;
-			}
-			if(options.fuzzy) {
-				matchmask = 1 << (patternLen - 1);
-				pattern_alphabet = (function () {
-					var mask = {},
-						i = 0;
-					for (i = 0; i < patternLen; i++) {
-						mask[pattern.charAt(i)] = 0;
-					}
-					for (i = 0; i < patternLen; i++) {
-						mask[pattern.charAt(i)] |= 1 << (patternLen - i - 1);
-					}
-					return mask;
-				}());
-				match_bitapScore = function (e, x) {
-					var accuracy = e / patternLen,
-						proximity = Math.abs(MATCH_LOCATION - x);
-					if(!MATCH_DISTANCE) {
-						return proximity ? 1.0 : accuracy;
-					}
-					return accuracy + (proximity / MATCH_DISTANCE);
-				};
-			}
-			search = function (text) {
-				text = options.caseSensitive ? text : text.toLowerCase();
-				if(pattern === text || text.indexOf(pattern) !== -1) {
-					return {
-						isMatch: true,
-						score: 0
-					};
-				}
-				if(!options.fuzzy) {
-					return {
-						isMatch: false,
-						score: 1
-					};
-				}
-				var i, j,
-					textLen = text.length,
-					scoreThreshold = MATCH_THRESHOLD,
-					bestLoc = text.indexOf(pattern, MATCH_LOCATION),
-					binMin, binMid,
-					binMax = patternLen + textLen,
-					lastRd, start, finish, rd, charMatch,
-					score = 1,
-					locations = [];
-				if (bestLoc !== -1) {
-					scoreThreshold = Math.min(match_bitapScore(0, bestLoc), scoreThreshold);
-					bestLoc = text.lastIndexOf(pattern, MATCH_LOCATION + patternLen);
-					if (bestLoc !== -1) {
-						scoreThreshold = Math.min(match_bitapScore(0, bestLoc), scoreThreshold);
-					}
-				}
-				bestLoc = -1;
-				for (i = 0; i < patternLen; i++) {
-					binMin = 0;
-					binMid = binMax;
-					while (binMin < binMid) {
-						if (match_bitapScore(i, MATCH_LOCATION + binMid) <= scoreThreshold) {
-							binMin = binMid;
-						} else {
-							binMax = binMid;
-						}
-						binMid = Math.floor((binMax - binMin) / 2 + binMin);
-					}
-					binMax = binMid;
-					start = Math.max(1, MATCH_LOCATION - binMid + 1);
-					finish = Math.min(MATCH_LOCATION + binMid, textLen) + patternLen;
-					rd = new Array(finish + 2);
-					rd[finish + 1] = (1 << i) - 1;
-					for (j = finish; j >= start; j--) {
-						charMatch = pattern_alphabet[text.charAt(j - 1)];
-						if (i === 0) {
-							rd[j] = ((rd[j + 1] << 1) | 1) & charMatch;
-						} else {
-							rd[j] = ((rd[j + 1] << 1) | 1) & charMatch | (((lastRd[j + 1] | lastRd[j]) << 1) | 1) | lastRd[j + 1];
-						}
-						if (rd[j] & matchmask) {
-							score = match_bitapScore(i, j - 1);
-							if (score <= scoreThreshold) {
-								scoreThreshold = score;
-								bestLoc = j - 1;
-								locations.push(bestLoc);
-								if (bestLoc > MATCH_LOCATION) {
-									start = Math.max(1, 2 * MATCH_LOCATION - bestLoc);
-								} else {
-									break;
-								}
-							}
-						}
-					}
-					if (match_bitapScore(i + 1, MATCH_LOCATION) > scoreThreshold) {
-						break;
-					}
-					lastRd = rd;
-				}
-				return {
-					isMatch: bestLoc >= 0,
-					score: score
-				};
-			};
-			return txt === true ? { 'search' : search } : search(txt);
-		};
-	}($));
 
 	// include the search plugin by default
 	// $.jstree.defaults.plugins.push("search");
