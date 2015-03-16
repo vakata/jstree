3aac28dea69a97ffc2c5b148b8556de8095c3e18
staygreengored <sggr90@gmail.com>
2015-03-16 17:36:42 +0300

    Fixed animation

--- dist/jstree.js
+++ dist/jstree.js
@@ -2596,11 +2596,10 @@
 				}
 				else {
 					d
-						.children(".jstree-children").attr("style","display:none !important").end()
+						.children(".jstree-children").attr("style","display:block !important").end()
 						.removeClass("jstree-open").addClass("jstree-closed").attr("aria-expanded", false)
 						.children(".jstree-children").stop(true, true).slideUp(animation, function () {
-							this.style.display = "";
-							//d.children('.jstree-children').remove();
+							d.children(".jstree-children").attr("style","display:none !important");
 							t.trigger("after_close", { "node" : obj });
 						});
 				}
