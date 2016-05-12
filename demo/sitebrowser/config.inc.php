87142cf072de1b769605fccc68e41af3515c1da4
LouisPhilippe <opencmii@users.noreply.github.com>
2016-05-12 14:21:49 -0400

    Update config.inc.php
    used phpmyadmin approach for defining parameters.
    Provides the capability to be extended to many model repository in the future

--- demo/sitebrowser/config.inc.php
+++ demo/sitebrowser/config.inc.php
@@ -1,4 +1,39 @@
-$db_name     = serr;
+<?php
+
+/*
+ * Modeler dbms configuration
+ */
+$i = 0;
+
+/*
+ * First server
+ */
+$i++;
+
+/* Authentication type and info */
+$db_name     = modeler;
 $db_host     = localhost;
-$db_user     = serr;
+$db_user     = modeler;
 $db_password = ;
+
+$cfg['Servers'][$i]['auth_type'] = 'config';
+$cfg['Servers'][$i]['user'] = 'modeler';
+$cfg['Servers'][$i]['password'] = '';
+$cfg['Servers'][$i]['extension'] = 'mysqli';
+$cfg['Servers'][$i]['AllowNoPassword'] = true;
+$cfg['Lang'] = 'en';
+
+/* Bind to the localhost ipv4 address and tcp */
+$cfg['Servers'][$i]['host'] = '127.0.0.1';
+$cfg['Servers'][$i]['connect_type'] = 'tcp';
+
+/* Advanced objtree features */
+$cfg['Servers'][$i]['a'] = 'tbd';
+$cfg['Servers'][$i]['b'] = 'tbd';
+$cfg['Servers'][$i]['c'] = 'tbd';
+
+/*
+ * End of Modeler servers configuration
+ */
+
+?>
