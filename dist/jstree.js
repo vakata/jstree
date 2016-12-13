57c071d91503a1ef4f0292c7c3bc1be8532ec0e3
Th3OnlyC0D3R <nico.fengler@gmx.de>
2016-12-13 09:13:40 +0100

    Update jstree.js

--- dist/jstree.js
+++ dist/jstree.js
@@ -5901,7 +5901,7 @@
 		this.bind = function () {
 			parent.bind.call(this);
 
-			var last_ts = 0, cto = null, ex, ey;
+			var last_ts = 0, cto = null, ex, ey, last_timeStamp;
 			this.element
 				.on("contextmenu.jstree", ".jstree-anchor", $.proxy(function (e, data) {
 						if (e.target.tagName.toLowerCase() === 'input') {
@@ -5929,6 +5929,7 @@
 						if(!e.originalEvent || !e.originalEvent.changedTouches || !e.originalEvent.changedTouches[0]) {
 							return;
 						}
+						last_timeStamp = e.originalEvent.timeStamp;
 						ex = e.originalEvent.changedTouches[0].clientX;
 						ey = e.originalEvent.changedTouches[0].clientY;
 						cto = setTimeout(function () {
@@ -5943,6 +5944,9 @@
 				.on('touchend.vakata.jstree', function (e) {
 						if(cto) {
 							clearTimeout(cto);
+							if ((e.originalEvent.timeStamp - last_timeStamp) > 650) {
+							    e.preventDefault();
+							}
 						}
 					});
 
