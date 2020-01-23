7164a07a6fb850083c8c0a3cb69e7be1ff1a4f39
arutyunyan <arutyunyan@bolid.ru>
2020-01-23 09:32:57 +0300

    build new one

--- dist/jstree.js
+++ dist/jstree.js
@@ -13,7 +13,7 @@
 }(function ($, undefined) {
 	"use strict";
 /*!
- * jsTree {{VERSION}}
+ * jsTree 3.3.8
  * http://jstree.com/
  *
  * Copyright (c) 2014 Ivan Bozhanov (http://vakata.com)
@@ -63,7 +63,7 @@
 		 * specifies the jstree version in use
 		 * @name $.jstree.version
 		 */
-		version : '{{VERSION}}',
+		version : '3.3.8',
 		/**
 		 * holds all the default options used when creating new instances
 		 * @name $.jstree.defaults
