af08c768e871c6db71800e76e692f5e3dee0470a
Ahmed El Moden <cresuso@gmail.com>
2020-08-04 17:24:44 +0200

    Load src/jstree when using a module bundler.

--- src/jstree.dnd.js
+++ src/jstree.dnd.js
@@ -7,10 +7,10 @@
 (function (factory) {
 	"use strict";
 	if (typeof define === 'function' && define.amd) {
-		define('jstree.dnd', ['jquery','jstree'], factory);
+		define('jstree.dnd', ['jquery','./jstree.js'], factory);
 	}
 	else if(typeof exports === 'object') {
-		factory(require('jquery'), require('jstree'));
+		factory(require('jquery'), require('./jstree.js'));
 	}
 	else {
 		factory(jQuery, jQuery.jstree);
