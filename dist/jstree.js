6256df013ebd98aea138402d8ac96db3efe0c0da
Ivan Bozhanov <git@vakata.com>
2024-09-03 10:13:38 +0300

    3.3.17

--- dist/jstree.js
+++ dist/jstree.js
@@ -13,7 +13,7 @@
 }(function ($, undefined) {
 	"use strict";
 /*!
- * jsTree 3.3.16
+ * jsTree 3.3.17
  * http://jstree.com/
  *
  * Copyright (c) 2014 Ivan Bozhanov (http://vakata.com)
@@ -63,7 +63,7 @@
 		 * specifies the jstree version in use
 		 * @name $.jstree.version
 		 */
-		version : '3.3.16',
+		version : '3.3.17',
 		/**
 		 * holds all the default options used when creating new instances
 		 * @name $.jstree.defaults
